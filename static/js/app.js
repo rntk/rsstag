78f616d6e5ab89816b6cc866eb624f16c78c0bbd
Rntk <kvrenat@gmail.com>
2014-11-08 23:00:16 +0600

    change workflow to async

--- static/js/app.js
+++ static/js/app.js
@@ -66,9 +66,13 @@ checkStatus: function() {
     }
 },
 
-readOne: function() {
+readOne: function(el_this) {
     var status_text = '';
-    if ($(this).hasClass('read')) {
+    var status = 0;
+    var promise = null;
+    var $this = $(el_this);
+    var app = this;
+    if ($this.hasClass('read')) {
         status = 0;
         status_text = 'unread';
     }
@@ -76,70 +80,74 @@ readOne: function() {
         status = 1;
         status_text = 'read';
     }
-    result = $.ajax({
+    promise = $.ajax({
         url: app.urls['mark_read_posts_url'],
         type: 'POST',
-        data: {'pos' : $(this).data('pos'), 'status': status},
+        data: {'pos' : $this.data('pos'), 'status': status},
         dataType: 'json',
-        /*success: function(data){alert(data['result']);},
-        error: function(data){alert(data['result']);},*/
-        async: false,
-    }).responseJSON;
-    if ((result != undefined) && (result['result'] == 'ok')) {
-        $(this).toggleClass('read unread')
-               .text(status_text);
-        //app.udpate_prev_next_links($('.page').data('tag'));
-    }
-    else {
-        alert('Error. Please try again later');
-    }
-    app.syncReadAllButton();
+        async: true
+    });
+    promise.done(function(result){
+        if ((result != undefined) && (result['result'] == 'ok')) {
+            $this.toggleClass('read unread')
+                 .text(status_text);
+            //app.udpate_prev_next_links($('.page').data('tag'));
+        }
+        else {
+            alert('Error. Please try again later');
+        }
+        app.syncReadAllButton();
+    });
 },
 
-readAll: function() {
-    var $this = $(this);
+readAll: function(el_this) {
+    var $this = $(el_this);
+    var app = this;
+    var promise = null;
+    var posts = Array();
+    var $posts = $('div.post');
+    var status = 0;
     if ($this.text() == 'read all') {
         status = 1;
     }
     else {
         status = 0;
     }
-    posts = Array();
-    $posts = $('div.post');
     for (i = 0; i < $posts.length; i++) {
         posts.push($($posts[i]).data('pos'));
     }
-    result = $.ajax({
+    promise = $.ajax({
         url: app.urls['mark_read_posts_url'],
         type: 'POST',
         data: {'posts': posts, 'status': status},
         dataType: 'json',
-        //success: function(data){alert(data['result']);},
-        //error: function(data){alert(data['result']);},
-        async: false,
-    }).responseJSON;
-    if ((result != undefined) && (result['result'] == 'ok')) {
-        $('span.read_button').each(function() {
+        async: true
+    });
+    promise.done(function(result) {
+        if ((result != undefined) && (result['result'] == 'ok')) {
+            $('span.read_button').each(function() {
+                var $this = $(this);
+                if (status == 1) {
+                    $this.toggleClass('read unread')
+                         .text('read');
+                }
+                else {
+                    $this.toggleClass('read unread')
+                         .text('unread');
+                }
+            });
             if (status == 1) {
-                $(this).toggleClass('read unread')
-                       .text('read');
+                $this.children('span').text('unread all');
             }
             else {
-                $(this).toggleClass('read unread')
-                       .text('unread');
+                $this.children('span').text('read all');
             }
-        });
-        if (status == 1) {
-            $this.children('span').text('unread all');
+            //app.udpate_prev_next_links($('.page').data('tag'));
         }
         else {
-            $this.children('span').text('read all');
+            alert('Error. Please try again later');
         }
-        //app.udpate_prev_next_links($('.page').data('tag'));
-    }
-    else {
-        alert('Error. Please try again later');
-    }
+    });
 },
 
 syncReadAllButton: function() {
@@ -152,8 +160,14 @@ syncReadAllButton: function() {
 },
 
 showContent: function(el, show_status) {
-    $content = $(el).parent().children(".post_content");
-    pos = $content.parent().data('pos');
+    var $content = $(el).parent().children(".post_content");
+    var pos = $content.parent().data('pos');
+    var app = this;
+    var all_ready = $.Deferred();
+    all_ready.done(function(){
+        $content.toggleClass('show hide');
+        app.scrollTop(app.$current_post);
+    });
     if (show_status == undefined) {
         if ($content.attr('class') == 'show') {
             show_status = false;
@@ -164,77 +178,104 @@ showContent: function(el, show_status) {
     }
     if (show_status) {
         if (!app.all_posts_content[pos]) {
-            app.loadPostContent(pos, false);
+            var promise = app.loadPostContent(pos);
+            promise.done(function(){
+                //$content.attr('class', 'show');
+                $content.html(app.all_posts_content[pos]);
+                all_ready.resolve();
+            });
         }
-        //$content.attr('class', 'show');
-        $content.html(app.all_posts_content[pos]);
+        else {
+            //$content.attr('class', 'show');
+            $content.html(app.all_posts_content[pos]);
+            all_ready.resolve();
+        }
     }
     else {
         //$content.attr('class', 'hide');
         $content.text('');
+        all_ready.resolve();
     }
-    $content.toggleClass('show hide');
-    app.scrollTop(app.$current_post);
+    return(all_ready);
+
 },
 
-showAll: function() {
+showAll: function(el_this) {
+    var $this = $(el_this);
+    var app = this;
+    var all_ready = $.Deferred();
+    var show_status = false;
+    all_ready.done(function(){
+        $('div.post_content').each(function(){
+            app.showContent(this, show_status);
+        });
+        app.all_showed = !app.all_showed;
+    });
     if (app.all_showed) {
         show_status = false;
-        $(this).children('span').text('show all');
+        $this.children('span').text('show all');
+        all_ready.resolve();
     }
     else {
-        if (!app.all_page_loaded) {
-            app.loadPostsContent(false);
+        if (app.all_page_loaded) {
+            show_status = true;
+            $this.children('span').text('hide all');
+            all_ready.resolve();
         }
-        show_status = true;
-        $(this).children('span').text('hide all');
+        else {
+            var promise = app.loadPostsContent();
+            promise.done(function() {
+                show_status = true;
+                $this.children('span').text('hide all');
+                all_ready.resolve();
+            });
+        }
     }
-    $('div.post_content').each(function(){
-        app.showContent(this, show_status);
-    });
-    app.all_showed = !app.all_showed;
+
 },
 
-udpate_prev_next_links: function(tag) {
+/*udpate_prev_next_links: function(tag) {
     links = $.ajax({
         url: app.urls['update_prev_next_link_url'] + '?tag=' + tag,
         type: 'GET',
         dataType: 'json',
-        /*success: function(data){alert(data['result']);},
-        error: function(data){alert(data['result']);},*/
-        async: false,
+        async: false
     }).responseJSON;
     if (links) {
         $('#next_tag').children('a').attr('href', links['next']);
         $('#prev_tag').children('a').attr('href', links['prev']);
     }
-},
+},*/
 
-loadPostContent: function(pos, async) {
-    content = $.ajax({
-        url: app.urls['post_content_url'],            
+loadPostContent: function(pos) {
+    var app = this;
+    var promise = $.ajax({
+        url: app.urls['post_content_url'],
         type: 'POST',
         data: {'postid': pos},
         dataType: 'json',
-        async: async,
-    }).responseJSON;
-    if ((content) && (content['result'] == 'ok')) {
-        app.all_posts_content[content['data']['pos']] = content['data']['content'];
-        app.all_posts_content['length']++;
-        if (!app.all_page_loaded) {
-            if (app.all_posts_content['length'] >= app.posts_count) {
-                app.all_page_loaded = true;
+        async: true
+    });
+    promise.done(function(content) {
+        if ((content) && (content['result'] == 'ok')) {
+            app.all_posts_content[content['data']['pos']] = content['data']['content'];
+            app.all_posts_content['length']++;
+            if (!app.all_page_loaded) {
+                if (app.all_posts_content['length'] >= app.posts_count) {
+                    app.all_page_loaded = true;
+                }
             }
         }
-    }
-    else {
-        alert('error');
-    }
+        else {
+            alert('error');
+        }
+    });
+    return(promise);
 },
 
-loadPostsContent: function(async) {
+loadPostsContent: function() {
     /*content = $.ajax({
-        url: app.urls['posts_content_url'],            
+        url: app.urls['posts_content_url'],
         type: 'POST',
         data: {'group': app.group, 'group_id': app.group_id, 'page': app.current_page},
         dataType: 'json',
@@ -253,31 +294,38 @@ loadPostsContent: function(async) {
     else {
         alert('error');
     }*/
-    while (!app.all_page_loaded) {
-        content = $.ajax({
-            url: app.urls['posts_content_url'],            
+    var app = this;
+    if (!app.all_page_loaded) {
+        var promise = $.ajax({
+            url: app.urls['posts_content_url'],
             type: 'POST',
             data: {'group': app.group, 'group_id': app.group_id, 'page': app.current_page},
             dataType: 'json',
-            async: async
-        }).responseJSON;
-        if ((content) && (content['result'] == 'ok')) {
-            for (i = 0; i < content['data'].length; i++) {
-                app.all_posts_content[content['data'][i]['pos']] = content['data'][i]['content'];
-            }
-            app.all_posts_content['length'] = content['data'].length;
-            if (content['page_count'] <= app.current_page) {
-                app.all_page_loaded = true;
+            async: true
+        });
+        promise.done(function(content){
+            if ((content) && (content['result'] == 'ok')) {
+                for (i = 0; i < content['data'].length; i++) {
+                    app.all_posts_content[content['data'][i]['pos']] = content['data'][i]['content'];
+                }
+                app.all_posts_content['length'] = content['data'].length;
+                if (content['page_count'] <= app.current_page) {
+                    app.all_page_loaded = true;
+                }
+                else {
+                    app.current_page++;
+                }
             }
             else {
-                app.current_page++;
+                alert('Error. Please try later');
+                app.all_page_loaded = true
             }
-        }
-        else {
-            alert('Error. Please try later');
-            app.all_page_loaded = true
-        }
+            if (!app.all_page_loaded) {
+                promise = app.loadPostsContent();
+            }
+        });
     }
+    return(promise);
 },
 
 setCurrentPost: function(current_post) {
@@ -311,45 +359,47 @@ hideToolbar: function() {
     this.$toolbar.hide();
 },
 
-setOnlyUnread : function() {
+setOnlyUnread: function(el_this) {
+    var app = this;
     if (app.$only_unread_checkbox.prop('checked')) {
         status = 1;
     }
     else {
         status = 0;
     }
-    $.ajax({
-        url: app.urls['only_unread_url'],            
+    var promise = $.ajax({
+        url: app.urls['only_unread_url'],
         type: 'POST',
         data: {'status': status},
         dataType: 'json',
-        success: function(content){
-            if ((content) && (content['result'] == 'ok')) {
-                app.$only_unread_checkbox.val(true);
-                if (window.location.pathname != '/') {
-                    window.location.reload();
-                }
+        async: true,
+    });
+    promise.done(function(content){
+        if ((content) && (content['result'] == 'ok')) {
+            app.$only_unread_checkbox.val(true);
+            if (window.location.pathname != '/') {
+                window.location.reload();
             }
-            else {
-                app.$only_unread_checkbox.prop('checked', !app.$only_unread_checkbox.prop('checked'));
-                alert('error');
-            }
-        },
-        async: false,
+        }
+        else {
+            app.$only_unread_checkbox.prop('checked', !app.$only_unread_checkbox.prop('checked'));
+            alert('error');
+        }
     });
 },
-showHelp: function() {
+showHelp: function(el_this) {
     var $help = $('#help_window');
     if ($help) {
         if ($help.css('display') === 'none') {
             $help.show();
-        }                        
+        }
         else {
             $help.hide();
         }
     }
 },
 showTags: function() {
+    var app = this;
     var el_array = $.parseHTML($.ajax({type: 'GET', url: $('#link_to_back').children('a').attr('href'), dataType: 'html', async: false}).responseText, undefined, false);
     var l_found = false;
     var c_found = false;
@@ -460,29 +510,33 @@ showSearchResult: function() {
         this.$search_result_window.hide();
     }
 },
-showPostLinks: function() {
-    var $this = $(this);
+showPostLinks: function(el_this) {
+    var $this = $(el_this);
+    var app = this;
     var postid = $this.parent().parent().data('pos');
     var result = undefined;
     var p_l = '';
+    var promise = null;
     if (app.post_links_id !== postid) {
-        result = $.ajax({type: 'POST', data: {'postid': postid}, url: app.urls['post_links_url'], dataType: 'json', async: false}).responseJSON;
-        if ((result) && (result['result'] == 'ok')) {
-            var data = result['data'];
-            p_l = '<a href="' + data['c_url'] + '">' + data['c_title'] + '</a> | <a href="' + data['f_url'] + '">' + data['f_title'] + '</a><br />';
-            //var d_length = data['tags'].length;
-            for (var i = 0; i < data['tags'].length; i++) {
-                p_l += '<a href="' + data['tags'][i]['url'] + '">' + data['tags'][i]['tag'] + '</a>, ';
+        promise = $.ajax({type: 'POST', data: {'postid': postid}, url: app.urls['post_links_url'], dataType: 'json', async: true});
+        promise.done(function(result){
+            if ((result) && (result['result'] == 'ok')) {
+                var data = result['data'];
+                p_l = '<a href="' + data['c_url'] + '">' + data['c_title'] + '</a> | <a href="' + data['f_url'] + '">' + data['f_title'] + '</a><br />';
+                //var d_length = data['tags'].length;
+                for (var i = 0; i < data['tags'].length; i++) {
+                    p_l += '<a href="' + data['tags'][i]['url'] + '">' + data['tags'][i]['tag'] + '</a>, ';
+                }
+                var offset = $this.offset();
+                app.post_links_id = postid;
+                app.$post_links.children('.post_links_content').html(p_l);
+                app.$post_links.css({'top': offset.top, 'left': offset.left})
+                               .show();
             }
-            var offset = $this.offset();
-            app.post_links_id = postid;
-            app.$post_links.children('.post_links_content').html(p_l);
-            app.$post_links.css({'top': offset.top, 'left': offset.left})
-                           .show();
-        }
-        else {
-            alert(result['data']);
-        }
+            else {
+                alert(result['data']);
+            }
+        });
     }
     else {
         app.$post_links.show();
@@ -494,7 +548,9 @@ showPostLinks: function() {
 $(document).ready(function() {
     if ($('#only_unread_checkbox').length > 0) {
         app.$only_unread_checkbox = $('#only_unread_checkbox');
-        app.$only_unread_checkbox.click(app.setOnlyUnread);
+        app.$only_unread_checkbox.click(function() {
+            app.setOnlyUnread(this);
+        });
     }
     $(document).ajaxStart(function() { $('#loading').show(); });
     $(document).ajaxStop(function() { $('#loading').hide(); });
@@ -559,11 +615,11 @@ $(document).ready(function() {
             app.startX = touch.pageX;
             app.startY = touch.pageY;
         });
-        $div_posts.on('touchmove', function(e){ 
+        $div_posts.on('touchmove', function(e){
             app.is_gesture = true;
 
         });
-        $div_posts.on('touchend', function(e){ 
+        $div_posts.on('touchend', function(e){
             touch = e.originalEvent.changedTouches[0];
             app.endX = touch.pageX;
             app.endY = touch.pageY;
@@ -576,7 +632,7 @@ $(document).ready(function() {
                 }
             }
         });
-        $div_posts.on('touchcancel', function(e){ 
+        $div_posts.on('touchcancel', function(e){
             touch = e.originalEvent.changedTouches[0];
             app.endX = touch.pageX;
             app.endY = touch.pageY;
@@ -603,13 +659,21 @@ $(document).ready(function() {
         }
         app.current_page = 1;
         app.syncReadAllButton();
-        $('a.post_show_links').click(app.showPostLinks);
+        $('a.post_show_links').click(function() {
+            app.showPostLinks(this);
+        });
         $('.post_links_close').on('click', function(){
             $(this).parent().hide();
         });
-        $('#help_button').on('click', app.showHelp);
-        $('span.read_button').on('click', app.readOne);
-        $('#read_all').click(app.readAll);
+        $('#help_button').on('click', function() {
+            app.showHelp(this);
+        });
+        $('span.read_button').on('click', function() {
+            app.readOne(this);
+        });
+        $('#read_all').click(function(){
+            app.readAll(this);
+        });
         $('a.post_show_content').click(function() {
             var $prnt = $(this).parent();
             app.setCurrentPost($prnt.parent());
@@ -617,12 +681,14 @@ $(document).ready(function() {
             return(false);
         });
         $('.post_title_link').click(function() {
-            self = $(this).parent().parent().find('.read_button');
+            var self = $(this).parent().parent().find('.read_button');
             if (self.hasClass('unread')) {
                 setTimeout(function() {self.click()}, 50);
             }
         });
-        $('#show_all').click(app.showAll);
+        $('#show_all').click(function(){
+            app.showAll(this);
+        });
         app.cloud_items_count = $('li.cloud_item').length;
         if (app.cloud_items_count != 0) {
             $('#cloud_items_count').text(app.cloud_items_count);
@@ -698,9 +764,9 @@ $(document).ready(function() {
                 }
             }
         });
-        /*$(document).scroll(function(){
+        /*$(document).scroll(function(){ //not async version, need change
             if (!app.all_page_loaded) {
-                
+
                 //app.loadPostsContent();
             }
         });*/
