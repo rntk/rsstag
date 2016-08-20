cb748b106ce462898f6a6d890ed8c4916d6779ad
Renat <kvrenat@gmail.com>
2016-08-20 15:31:38 +0600

    fix html cleaner

--- tests/test_html_cleaner.py
+++ tests/test_html_cleaner.py
@@ -8,10 +8,14 @@ class TestHTMLCleaner(unittest.TestCase):
         txt = 'test text'
         html = '<body><span>{}</span></body>'.format(txt)
         cleaner.feed(html)
+        self.assertEqual(cleaner.get_content(), [txt])
         txt1 = 'test text1'
         html = '<body><span>{}<a href="#">{}</a></span></body>'.format(txt, txt1)
         cleaner.feed(html)
-        self.assertEqual(cleaner.strings, [txt, txt, txt1])
+        self.assertEqual(cleaner.get_content(), [txt, txt, txt1])
+        cleaner.purge();
+        self.assertEqual(cleaner.get_content(), [])
+        self.assertNotEqual(cleaner.get_content(), [txt, txt, txt1])
 
 
 if __name__ == '__main__':
