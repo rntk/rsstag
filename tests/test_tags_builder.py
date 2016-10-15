d90f9038caeb58b7d7173f9820ec966bfb7bc5bb
Rnt <kvrenat@gmail.com>
2016-10-15 22:15:39 +0600

    add test for bi-grams builder

--- tests/test_tags_builder.py
+++ tests/test_tags_builder.py
@@ -8,7 +8,7 @@ class TestTagsBuilder(unittest.TestCase):
         text = 'тестировали тестировала тестировал testing tested оно 2016 Pokémon'
         builder.build_tags(text)
         tags = builder.get_tags()
-        expect_tags = ['тестировать', 'test', 'оно', '2016']
+        expect_tags = ['тестировать', 'test', 'оно', '2016', 'pokémon']
         self.assertEqual(tags.sort(), expect_tags.sort())
         words = builder.get_words()
         self.assertEqual(words, {
@@ -41,6 +41,33 @@ class TestTagsBuilder(unittest.TestCase):
         for i, word in enumerate(words):
             self.assertEqual(builder.process_word(word), expect[i])
 
-
+    def test_build_bi_grams(self):
+        builder = TagsBuilder('[^\w\d ]')
+        text = 'тестировали тестировала тестировал testing tested оно 2016 Pokémon'
+        builder.build_bi_grams(text)
+        tags = builder.get_bi_grams()
+        expect_tags = {
+            'тестировать тестировать': set(['тестировать', 'тестировать']),
+            'тестировать test': set(['тестировать', 'test']),
+            'test test': set(['test', 'test']),
+            'test оно': set(['test', 'оно']),
+            'оно 2016': set(['оно', '2016']),
+            '2016 pokémon': set(['2016', 'pokémon'])
+        }
+        self.assertEqual(tags, expect_tags)
+        words = builder.get_bi_grams_words()
+        self.assertEqual(words, {
+            'тестировать тестировать': set(['тестировали', 'тестировала', 'тестировал']),
+            'тестировать test': set(['тестировал', 'testing']),
+            'test test': set(['testing', 'tested']),
+            'test оно': set(['tested', 'оно']),
+            'оно 2016': set(['оно', '2016']),
+            '2016 pokémon': set(['2016', 'pokémon'])
+        })
+        builder.purge()
+        tags = builder.get_bi_grams()
+        self.assertEqual(tags, {})
+        words = builder.get_bi_grams_words()
+        self.assertEqual(words, {})
 if __name__ == '__main__':
     unittest.main()
