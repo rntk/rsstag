92b99dee8182914be0ea34079f874fc24c1165de
Rnt <kvrenat@gmail.com>
2016-10-30 17:16:09 +0600

    rename entity extractor class, add morphy

--- entity.py
+++ entity.py
@@ -1,13 +1,13 @@
 import gzip
 from rsstag.utils import load_config
-from rsstag.entity_extractor import RssTagEntityExtractor
+from rsstag.entity_extractor import EntityExtractor
 from pymongo import MongoClient
 
 if __name__ == '__main__':
     config = load_config('./rsscloud.conf')
     cl = MongoClient(config['settings']['db_host'], int(config['settings']['db_port']))
     db = cl.rss
-    ent_ex = RssTagEntityExtractor()
+    ent_ex = EntityExtractor()
     cur = db.posts.find({})
     for post in cur:
         text = post['content']['title'] + ' ' + gzip.decompress(post['content']['content']).decode('utf-8', 'replace')
--- rsstag/entity_extractor.py
+++ rsstag/entity_extractor.py
@@ -1,22 +1,33 @@
 from typing import List, Iterator
-from unicodedata import category as get_letter_category
+import re
+import unicodedata
+from rsstag.html_cleaner import HTMLCleaner
+import pymorphy2
 import nltk
 
-class RssTagEntityExtractor:
+class EntityExtractor:
     def __init__(self):
+        self._html_cleaner = HTMLCleaner()
+        self._stemmer_ru = pymorphy2.MorphAnalyzer()
+        self._stemmer_en = nltk.stem.PorterStemmer()
+        self._only_cyrillic = re.compile('[А-яЁё-]')
+        self._only_latin = re.compile('[A-z-]')
+        self._delimiter = ' '
         self._stopwords = set(nltk.corpus.stopwords.words('english') + nltk.corpus.stopwords.words('russian'))
-        pass
 
     def find_geo_entities(self, entities: List[str]) -> List[str]:
         pass
 
     def tokenize_text(self, text: str) -> Iterator:
+        self._html_cleaner.purge()
+        self._html_cleaner.feed(text)
+        text = ' '.join(self._html_cleaner.get_content())
         token = ''
         sign = ''
         delimiter = False
         add_to_token = False
         for letter in text:
-            letter_category = get_letter_category(letter)
+            letter_category = unicodedata.category(letter)
             if (letter_category == 'Ll') or (letter_category == 'Lu') or (letter_category == 'Nd'):
                 add_to_token = True
             elif letter.isspace():
@@ -38,7 +49,33 @@ class RssTagEntityExtractor:
                     yield sign
                     sign = ''
 
-    def extract_entities(self, text: str, delimiter='_') -> List[str]:
+    def treat_entities(self, entities: List[list]) -> List[str]:
+        new_entities = []
+        for entity in entities:
+            new_entity = []
+            for word in entity:
+                word = word.casefold()
+                new_word = ''
+                if len(word) > 2:
+                    if self._only_cyrillic.match(word):
+                        morphy = self._stemmer_ru.parse(word)
+                        if morphy:
+                            if (morphy[0].tag.POS == 'NOUN') and (morphy[0].tag.number == 'sing'):
+                                new_word = morphy[0].normal_form
+                    elif self._only_latin.match(word):
+                        new_word = self._stemmer_en.stem(word)
+                    else:
+                        new_word = word
+                else:
+                    new_word = word
+                if new_word:
+                    new_entity.append(new_word)
+            if new_entity:
+                new_entities.append(new_entity)
+
+        return new_entities
+
+    def extract_entities(self, text: str) -> List[str]:
         entities = []
         entity = []
         add_to_entity = False
@@ -55,7 +92,10 @@ class RssTagEntityExtractor:
                 add_to_entity = True
                 initial = ''
             elif entity:
-                entities.append(' '.join(entity))
+                if entity[-1] != '.':
+                    entities.append(entity)
+                else:
+                    entities.append(entity[:-1])
                 entity = []
 
             if add_to_entity:
@@ -63,6 +103,11 @@ class RssTagEntityExtractor:
                 add_to_entity = False
 
         if entity:
-            entities.append(' '.join(entity))
+            if entity[-1] != '.':
+                entities.append(entity)
+            else:
+                entities.append(entity[:-1])
+
+        entities = self.treat_entities(entities)
 
         return entities
