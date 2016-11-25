f9152da9ca0d16f92a16f6198788aad5ae998813
Rnt <kvrenat@gmail.com>
2016-11-25 21:50:57 +0600

    fix for changed W2VLearn

--- w2v.py
+++ w2v.py
@@ -1,10 +1,15 @@
 import sys
+from rsstag.utils import load_config
 from rsstag.w2v import W2VLearn
+from pymongo import MongoClient
 
 if __name__ == '__main__':
     config_path = 'rsscloud.conf'
     if len(sys.argv) > 1:
         config_path = sys.argv[1]
-    learn = W2VLearn(config_path)
+    config = load_config(config_path)
+    cl = MongoClient(config['settings']['db_host'], int(config['settings']['db_port']))
+    db = cl[config['settings']['db_name']]
+    learn = W2VLearn(db, config)
     learn.fetch_texts()
     learn.learn()
