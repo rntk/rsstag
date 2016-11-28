94bbffcd5a30d365b91b5036a27b195af7b7b1df
Rnt <kvrenat@gmail.com>
2016-11-28 10:23:35 +0600

    support work under uswgi

---
+++ uwsgi.py
@@ -0,0 +1,10 @@
+from rsstag.web import RSSTagApplication
+
+config_path = 'rsscloud.conf'
+app = RSSTagApplication(config_path)
+print('out appl', __name__)
+
+def application(env, start_response):
+    print('in appl', __name__)
+    return app.setResponse(env, start_response)
+
