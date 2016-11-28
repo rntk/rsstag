480a2c4adcd6bd13d34717361bd5a4751e9b4ae6
Rnt <kvrenat@gmail.com>
2016-11-28 20:12:46 +0600

    add letters building

---
+++ make_letters.py
@@ -0,0 +1,31 @@
+import sys
+from rsstag.routes import RSSTagRoutes
+from rsstag.letters import RssTagLetters
+from rsstag.utils import load_config
+from rsstag.tags import RssTagTags
+from pymongo import MongoClient
+
+def make_letters(db, config):
+    router = RSSTagRoutes(config['settings']['host_name'])
+    user = db.users.find_one({})
+    letters = RssTagLetters(db)
+    tags = RssTagTags(db)
+    all_tags = tags.get_all(user['sid'], projection={'tag': True, 'unread_count': True})
+    result = False
+    if tags:
+        result = letters.sync_with_tags(user['sid'], all_tags, router)
+
+    return result
+
+if __name__ == '__main__':
+    config_path = 'rsscloud.conf'
+    if len(sys.argv) > 1:
+        config_path = sys.argv[1]
+    config = load_config(config_path)
+    cl = MongoClient(config['settings']['db_host'], int(config['settings']['db_port']))
+    db = cl[config['settings']['db_name']]
+    result = make_letters(db, config)
+    if result:
+        print('Done')
+    else:
+        print('Not done, result - ', result)
--- rsstag/letters.py
+++ rsstag/letters.py
@@ -1,25 +1,27 @@
 import logging
-from typing import Optional
+from collections import defaultdict
+from typing import Optional, List
+from rsstag.routes import RSSTagRoutes
 from rsstag.utils import getSortedDictByAlphabet
 from pymongo import MongoClient
 
 class RssTagLetters:
     indexes = ['owner']
     def __init__(self, db: MongoClient) -> None:
-        self.db = db
-        self.log = logging.getLogger('letters')
+        self._db = db
+        self._log = logging.getLogger('letters')
 
     def prepare(self) -> None:
         for index in self.indexes:
             try:
-                self.db.letters.create_index(index)
+                self._db.letters.create_index(index)
             except Exception as e:
-                self.log.warning('Can`t create index %s. May be already exists. Info: %s', index, e)
+                self._log.warning('Can`t create index %s. May be already exists. Info: %s', index, e)
 
     def get(self, owner: str, make_sort: bool=False) -> Optional[dict]:
         query = {'owner': owner}
         try:
-            letters = self.db.letters.find_one(query)
+            letters = self._db.letters.find_one(query)
             if letters:
                 if make_sort:
                     letters['letters'] = getSortedDictByAlphabet(letters['letters'])
@@ -27,7 +29,7 @@ class RssTagLetters:
             else:
                 result = {}
         except Exception as e:
-            self.log.error('Can`t get letters for user %s. Info: %s', owner, e)
+            self._log.error('Can`t get letters for user %s. Info: %s', owner, e)
             result = None
 
         return result
@@ -49,10 +51,31 @@ class RssTagLetters:
             inc_query[key] = -letters[letter] if readed else letters[letter]
         if find_query:
             try:
-                update_result = self.db.letters.update_one(find_query, {'$inc': inc_query})
+                update_result = self._db.letters.update_one(find_query, {'$inc': inc_query})
                 result = (update_result.matched_count > 0)
             except Exception as e:
-                self.log.error('Can`t change letters unread. User %s. Info: %s', owner, e)
+                self._log.error('Can`t change letters unread. User %s. Info: %s', owner, e)
                 result = None
 
+        return result
+
+    def sync_with_tags(self, owner: str, tags: List[dict], router: RSSTagRoutes) -> Optional[bool]:
+        letters = defaultdict(lambda: {'unread_count': 0, 'letter': ''})
+        for tag in tags:
+            letter = tag['tag'][0]
+            if letter not in letters:
+                letters[letter]['letter'] = letter
+                letters[letter]['local_url'] = router.getUrlByEndpoint(
+                    endpoint = 'on_group_by_tags_startwith_get',
+                    params = {'letter': letter, 'page_number': 1}
+                )
+            letters[letter]['unread_count'] += tag['unread_count']
+
+        try:
+            update_result = self._db.letters.update_one({'owner': owner}, {'$set': {'letters': letters}})
+            result = True #needd check matched_count/modified_count
+        except Exception as e:
+            self._log.error('Can`t change letters unread. User %s. Info: %s', owner, e)
+            result = None
+        
         return result
