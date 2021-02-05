cc62849e8cc63e7c80b07b60dbd8acadd6dbe869
rnt <kvrenat@gmail.com>
2021-02-05 16:22:11 +0600

    fix new webpack pipeline

--- static/js/webpack.config.js
+++ static/js/webpack.config.js
@@ -19,36 +19,37 @@ module.exports = {
     devtool: 'cheap-module-source-map',
     entry: path.join(__dirname, 'apps', 'app.js'),
     module: {
-        loaders: [
+        rules: [
             {
                 test: /\.js/,
-                loader: 'babel-loader',
                 include: path.join(__dirname, 'components'),
-                query: {
-                    //presets: ['react', 'es2015']
-                    presets: ['react'],
-                    plugins: ['transform-es2015-modules-umd']
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ["react", 'babel-preset-env']
+                    }
                 }
             },
             {
                 test: /\.js/,
-                loader: 'babel-loader',
                 include: [path.join(__dirname, 'storages'), path.join(__dirname, 'libs')],
-                query: {
-                    //presets: ['es2015']
-                    plugins: ['transform-es2015-modules-umd']
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['babel-preset-env']
+                    }
                 }
             },
             {
                 test: /app.js/,
-                loader: 'babel-loader',
                 include: path.join(__dirname, 'apps'),
-                query: {
-                    //presets: ['react', 'es2015']
-                    presets: ['react'],
-                    plugins: ['transform-es2015-modules-umd']
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['react', 'babel-preset-env'],
+                    }
                 }
-            },
+            }
         ]
     },
     plugins: plugins,
