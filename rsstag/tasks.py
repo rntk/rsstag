24ffda14f2c8996e55920c6d0d9680815369607d
rnt <kvrenat@gmail.com>
2022-01-07 21:18:59 +0600

    refactoring: decrease get_tasks function folding

--- rsstag/tasks.py
+++ rsstag/tasks.py
@@ -136,159 +136,160 @@ class RssTagTasks:
                 {"processing": TASK_NOT_IN_PROCESSING},
                 {"$set": {"processing": time.time()}},
             )
-            if user_task:
-                task["_id"] = user_task["_id"]
-                data = user_task
-                task["user"] = users.get_by_sid(user_task["user"])
-                if task["user"]:
-                    task["type"] = user_task["type"]
-                    if user_task["type"] == TASK_TAGS:
-                        data = []
-                        ps = self._db.posts.find(
-                            {
-                                "owner": task["user"]["sid"],
-                                "tags": [],
-                                "processing": POST_NOT_IN_PROCESSING,
-                            }
-                        ).limit(self._posts_bath_size)
-                        ids = []
-                        for p in ps:
-                            data.append(p)
-                            ids.append(p["_id"])
-                        unlock_task = True
-                        if ids:
-                            self._db.posts.update_many(
-                                {"_id": {"$in": ids}},
-                                {"$set": {"processing": time.time()}},
-                            )
-                        else:
-                            task["type"] = TASK_NOOP
-                            psc = self._db.posts.count_documents(
-                                {
-                                    "owner": task["user"]["sid"],
-                                    "tags": [],
-                                }
-                            )
-                            if psc == 0:
-                                if self.add_next_tasks(
-                                    task["user"]["sid"], user_task["type"]
-                                ):
-                                    self._db.tasks.remove({"_id": user_task["_id"]})
-                                    unlock_task = False
-                        if unlock_task:
-                            self._db.tasks.update_one(
-                                {"_id": user_task["_id"]},
-                                {"$set": {"processing": TASK_NOT_IN_PROCESSING}},
-                            )
-                    elif user_task["type"] == TASK_BIGRAMS_RANK:
-                        data = []
-                        bis_dt = self._db.bi_grams.find(
-                            {
-                                "owner": task["user"]["sid"],
-                                "temperature": 0,
-                                "processing": BIGRAM_NOT_IN_PROCESSING,
-                            },
-                            projection={"tag": True, "posts_count": True},
-                        ).limit(self._bigrams_bath_size)
-                        ids = []
-                        for bi_dt in bis_dt:
-                            data.append(bi_dt)
-                            ids.append(bi_dt["_id"])
-                        if ids:
-                            self._db.bi_grams.update_many(
-                                {"_id": {"$in": ids}},
-                                {"$set": {"processing": time.time()}},
-                            )
-                            self._db.tasks.update_one(
-                                {"_id": user_task["_id"]},
-                                {"$set": {"processing": TASK_NOT_IN_PROCESSING}},
-                            )
-                        else:
-                            task["type"] = TASK_NOOP
-                            self._db.tasks.remove({"_id": user_task["_id"]})
-                            self.add_next_tasks(
-                                task["user"]["sid"], user_task["type"]
-                            )
-                    elif user_task["type"] == TASK_TAGS_RANK:
-                        data = []
-                        tags_dt = self._db.tags.find(
-                            {
-                                "owner": task["user"]["sid"],
-                                "temperature": 0,
-                                "processing": TAG_NOT_IN_PROCESSING,
-                            },
-                            projection={"tag": True, "posts_count": True, "freq": True},
-                        ).limit(self._tags_bath_size)
-                        ids = []
-                        for tag_dt in tags_dt:
-                            data.append(tag_dt)
-                            ids.append(tag_dt["_id"])
-                        if ids:
-                            self._db.tags.update_many(
-                                {"_id": {"$in": ids}},
-                                {"$set": {"processing": time.time()}},
-                            )
-                            self._db.tasks.update_one(
-                                {"_id": user_task["_id"]},
-                                {"$set": {"processing": TASK_NOT_IN_PROCESSING}},
-                            )
-                        else:
-                            task["type"] = TASK_NOOP
-                            self._db.tasks.remove({"_id": user_task["_id"]})
-                            self.add_next_tasks(
-                                task["user"]["sid"], user_task["type"]
-                            )
-                    elif user_task["type"] == TASK_NER:
-                        data = []
-                        ps = self._db.posts.find(
-                            {
-                                "owner": task["user"]["sid"],
-                                "ner": {"$exists": False},
-                                "processing": POST_NOT_IN_PROCESSING,
-                            }
-                        ).limit(self._posts_bath_size)
-                        ids = []
-                        for p in ps:
-                            data.append(p)
-                            ids.append(p["_id"])
-                        unlock_task = True
-                        if ids:
-                            self._db.posts.update_many(
-                                {"_id": {"$in": ids}},
-                                {"$set": {"processing": time.time()}},
-                            )
-                        else:
-                            task["type"] = TASK_NOOP
-                            psc = self._db.posts.count_documents(
-                                {
-                                    "owner": task["user"]["sid"],
-                                    "ner": {"$exists": False}
-                                }
-                            )
-                            if psc == 0:
-                                if self.add_next_tasks(
-                                    task["user"]["sid"], user_task["type"]
-                                ):
-                                    self._db.tasks.remove({"_id": user_task["_id"]})
-                                    unlock_task = False
-                        if unlock_task:
-                            self._db.tasks.update_one(
-                                {"_id": user_task["_id"]},
-                                {"$set": {"processing": TASK_NOT_IN_PROCESSING}},
-                            )
+            if not user_task:
+                return task
 
-                    """if task_type == TASK_WORDS:
-                        if task['type'] == TASK_NOOP:
-                            data = db.tags.find_one_and_update(
-                                {
-                                    'processing': TASK_NOT_IN_PROCESSING,
-                                    'worded': {'$exists': False}
-                                },
-                                {'$set': {'processing': time.time()}}
-                            )
-                            if data and (data['processing'] == TASK_NOT_IN_PROCESSING):
-                                task['type'] = TASK_WORDS"""
-                    task["data"] = data
+            user = users.get_by_sid(user_task["user"])
+            if not user:
+                return task
+
+            data = user_task
+            task["user"] = user
+            task["_id"] = user_task["_id"]
+            task["type"] = user_task["type"]
+            if user_task["type"] == TASK_TAGS:
+                data = []
+                ps = self._db.posts.find(
+                    {
+                        "owner": task["user"]["sid"],
+                        "tags": [],
+                        "processing": POST_NOT_IN_PROCESSING,
+                    }
+                ).limit(self._posts_bath_size)
+                ids = []
+                for p in ps:
+                    data.append(p)
+                    ids.append(p["_id"])
+                unlock_task = True
+                if ids:
+                    self._db.posts.update_many(
+                        {"_id": {"$in": ids}},
+                        {"$set": {"processing": time.time()}},
+                    )
+                else:
+                    task["type"] = TASK_NOOP
+                    psc = self._db.posts.count_documents(
+                        {
+                            "owner": task["user"]["sid"],
+                            "tags": [],
+                        }
+                    )
+                    if psc == 0:
+                        if self.add_next_tasks(task["user"]["sid"], user_task["type"]):
+                            self._db.tasks.remove({"_id": user_task["_id"]})
+                            unlock_task = False
+                if unlock_task:
+                    self._db.tasks.update_one(
+                        {"_id": user_task["_id"]},
+                        {"$set": {"processing": TASK_NOT_IN_PROCESSING}},
+                    )
+            elif user_task["type"] == TASK_BIGRAMS_RANK:
+                data = []
+                bis_dt = self._db.bi_grams.find(
+                    {
+                        "owner": task["user"]["sid"],
+                        "temperature": 0,
+                        "processing": BIGRAM_NOT_IN_PROCESSING,
+                    },
+                    projection={"tag": True, "posts_count": True},
+                ).limit(self._bigrams_bath_size)
+                ids = []
+                for bi_dt in bis_dt:
+                    data.append(bi_dt)
+                    ids.append(bi_dt["_id"])
+                if ids:
+                    self._db.bi_grams.update_many(
+                        {"_id": {"$in": ids}},
+                        {"$set": {"processing": time.time()}},
+                    )
+                    self._db.tasks.update_one(
+                        {"_id": user_task["_id"]},
+                        {"$set": {"processing": TASK_NOT_IN_PROCESSING}},
+                    )
+                else:
+                    task["type"] = TASK_NOOP
+                    self._db.tasks.remove({"_id": user_task["_id"]})
+                    self.add_next_tasks(
+                        task["user"]["sid"], user_task["type"]
+                    )
+            elif user_task["type"] == TASK_TAGS_RANK:
+                data = []
+                tags_dt = self._db.tags.find(
+                    {
+                        "owner": task["user"]["sid"],
+                        "temperature": 0,
+                        "processing": TAG_NOT_IN_PROCESSING,
+                    },
+                    projection={"tag": True, "posts_count": True, "freq": True},
+                ).limit(self._tags_bath_size)
+                ids = []
+                for tag_dt in tags_dt:
+                    data.append(tag_dt)
+                    ids.append(tag_dt["_id"])
+                if ids:
+                    self._db.tags.update_many(
+                        {"_id": {"$in": ids}},
+                        {"$set": {"processing": time.time()}},
+                    )
+                    self._db.tasks.update_one(
+                        {"_id": user_task["_id"]},
+                        {"$set": {"processing": TASK_NOT_IN_PROCESSING}},
+                    )
+                else:
+                    task["type"] = TASK_NOOP
+                    self._db.tasks.remove({"_id": user_task["_id"]})
+                    self.add_next_tasks(
+                        task["user"]["sid"], user_task["type"]
+                    )
+            elif user_task["type"] == TASK_NER:
+                data = []
+                ps = self._db.posts.find(
+                    {
+                        "owner": task["user"]["sid"],
+                        "ner": {"$exists": False},
+                        "processing": POST_NOT_IN_PROCESSING,
+                    }
+                ).limit(self._posts_bath_size)
+                ids = []
+                for p in ps:
+                    data.append(p)
+                    ids.append(p["_id"])
+                unlock_task = True
+                if ids:
+                    self._db.posts.update_many(
+                        {"_id": {"$in": ids}},
+                        {"$set": {"processing": time.time()}},
+                    )
+                else:
+                    task["type"] = TASK_NOOP
+                    psc = self._db.posts.count_documents(
+                        {
+                            "owner": task["user"]["sid"],
+                            "ner": {"$exists": False}
+                        }
+                    )
+                    if psc == 0:
+                        if self.add_next_tasks(task["user"]["sid"], user_task["type"]):
+                            self._db.tasks.remove({"_id": user_task["_id"]})
+                            unlock_task = False
+                if unlock_task:
+                    self._db.tasks.update_one(
+                        {"_id": user_task["_id"]},
+                        {"$set": {"processing": TASK_NOT_IN_PROCESSING}},
+                    )
+
+            """if task_type == TASK_WORDS:
+                if task['type'] == TASK_NOOP:
+                    data = db.tags.find_one_and_update(
+                        {
+                            'processing': TASK_NOT_IN_PROCESSING,
+                            'worded': {'$exists': False}
+                        },
+                        {'$set': {'processing': time.time()}}
+                    )
+                    if data and (data['processing'] == TASK_NOT_IN_PROCESSING):
+                        task['type'] = TASK_WORDS"""
+            task["data"] = data
         except Exception as e:
             task["type"] = TASK_NOOP
             self._log.error("Worker can`t get tasks: %s", e)
