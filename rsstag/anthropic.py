d4dc783d1c1da03a395617dadd2c834660c568d7
rntk <kvrenat@gmail.com>
2025-01-12 20:17:56 +0100

    update antropic model

--- rsstag/anthropic.py
+++ rsstag/anthropic.py
@@ -9,7 +9,7 @@ class Anthropic:
         '''"claude-3-opus-20240229",
         "claude-3-sonnet-20240229",
         "claude-3-haiku-20240307",'''
-        self.__model = "claude-3-opus-20240229"
+        self.__model = "claude-3-5-haiku-20241022"
         self.__client = anthropic.Anthropic(
             api_key=token,
         )
