2fccc9fe4ad07cf3778efc487143bec3e07ea902
rntk <kvrenat@gmail.com>
2025-01-25 11:53:21 +0100

    llamacpp embeddings

--- rsstag/llamacpp.py
+++ rsstag/llamacpp.py
@@ -1,5 +1,5 @@
 import json
-from typing import List, Union
+from typing import List, Union, Optional
 import logging
 from urllib.parse import urlparse
 from http.client import HTTPConnection, HTTPSConnection
@@ -38,3 +38,32 @@ class LLamaCPP:
             return HTTPSConnection(self.__host)
         else:
             return HTTPConnection(self.__host)
+
+    def embeddings(self, texts: List[str]) -> Optional[List[List[float]]]:
+        conn = self.get_connection()
+        body = json.dumps(
+            {
+                #"model":"GPT-4",
+                "model":"text-embedding-3-small",
+                "encoding_format": "float",
+                "input": texts
+            }
+        )
+        headers = {
+            'Content-type': 'application/json',
+            'Authorization': 'Bearer '
+        }
+        conn.request("POST", "/v1/embeddings", body, headers)
+        res = conn.getresponse()
+        resp_body = res.read()
+        #logging.info("server response: %s", resp_body)
+        if res.status != 200:
+            err_msg = f"{res.status} - {res.reason} - {resp_body}"
+            logging.error(err_msg)
+            return None
+        resp = json.loads(resp_body)
+        embeds = []
+        for emb in resp["data"]:
+            embeds.append(emb["embedding"])
+
+        return embeds
