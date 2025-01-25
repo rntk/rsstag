4d0dd8669430ede521c47124d201f338ffb53fa6
rntk <kvrenat@gmail.com>
2025-01-25 11:52:40 +0100

    test anthropic citation

--- rsstag/anthropic.py
+++ rsstag/anthropic.py
@@ -32,3 +32,82 @@ class Anthropic:
         logging.info("OpenAI response: %s", resp)
 
         return resp.content[0].text
+
+    def call_citation(self, user_prompt: str, docs: list[str]) -> str:
+        messages = []
+        for i, msg in enumerate(docs):
+            messages.append({
+                "type": "document",
+                "source": {
+                    "type": "text",
+                    "media_type": "text/plain",
+                    "data": msg,
+                },
+                "title": f"Document {i}",
+                "context": msg,
+                "citations": {"enabled": True},
+            })
+        messages.append({
+            "type": "text",
+            "text": user_prompt,
+        })
+
+        try:
+            resp = self.__client.messages.create(
+                model=self.__model,
+                max_tokens=1024,
+                messages=[{"role": "user", "content": messages}],
+            )
+        except Exception as e:
+            logging.error("OpenAI error: %s", e)
+            return f"OpenAI error {e}"
+
+        logging.info("OpenAI response: %s", resp)
+        '''
+{
+    "content": [
+        {
+            "type": "text",
+            "text": "According to the document, "
+        },
+        {
+            "type": "text",
+            "text": "the grass is green",
+            "citations": [{
+                "type": "char_location",
+                "cited_text": "The grass is green.",
+                "document_index": 0,
+                "document_title": "Example Document",
+                "start_char_index": 0,
+                "end_char_index": 20
+            }]
+        },
+        {
+            "type": "text",
+            "text": " and "
+        },
+        {
+            "type": "text",
+            "text": "the sky is blue",
+            "citations": [{
+                "type": "char_location",
+                "cited_text": "The sky is blue.",
+                "document_index": 0,
+                "document_title": "Example Document",
+                "start_char_index": 20,
+                "end_char_index": 36
+            }]
+        }
+    ]
+}
+        '''
+
+        response = []
+        for item in resp.content:
+            if item.type == "text":
+                response.append(item.text)
+                if item.citations:
+                    for citation in item.citations:
+                        response.append("<citation>" + citation.cited_text + "</citation>")
+
+        return "\n".join(response)
--- rsstag/web/openai.py
+++ rsstag/web/openai.py
@@ -10,7 +10,7 @@ from rsstag.html_cleaner import HTMLCleaner
 
 from werkzeug.wrappers import Response, Request
 
-def on_openai_post(app: "RSSTagApplication", user: dict, rqst: Request):
+def on_openai_post_(app: "RSSTagApplication", user: dict, rqst: Request):
     data = rqst.get_json()
     if not data:
         return Response(json.dumps({"error": "No data"}), mimetype="application/json", status=400)
@@ -64,3 +64,49 @@ Your task is to process these messages and assist the user with the following re
 
     return Response(json.dumps(result), mimetype="application/json", status=200)
 
+def on_openai_post(app: "RSSTagApplication", user: dict, rqst: Request):
+    data = rqst.get_json()
+    if not data:
+        return Response(json.dumps({"error": "No data"}), mimetype="application/json", status=400)
+
+    tag = data["tag"]
+    if not tag:
+        return Response(json.dumps({"error": "No tag"}), mimetype="application/json", status=400)
+
+    if user["settings"]["only_unread"]:
+        only_unread = user["settings"]["only_unread"]
+    else:
+        only_unread = None
+    db_posts_c = app.posts.get_by_tags(user["sid"], [tag], only_unread)
+    cleaner = HTMLCleaner()
+    docs = []
+    for post in db_posts_c[:10]:
+        txt = post["content"]["title"] + ". " + gzip.decompress(post["content"]["content"]).decode(
+            "utf-8", "replace"
+        )
+        txt = txt.strip()
+        cleaner.purge()
+        cleaner.feed(txt)
+        txt = " ".join(cleaner.get_content())
+        docs.append(txt)
+
+    if not docs:
+        result = {"error": "No texts"}
+        return Response(json.dumps(result), mimetype="application/json", status=200)
+
+    user_msgs = ""
+    if "user"  in data and data["user"]:
+        user_msgs = data["user"]
+    if not user_msgs:
+        result = {"error": "No user messages"}
+        return Response(json.dumps(result), mimetype="application/json", status=400)
+    system_msg = f"""You are provided with a list of messages, each containing a keyword "{tag}".  
+Your task is to process these messages and assist the user with the following request: 
+{user_msgs}
+"""
+
+    #txt = app.llamacpp.call([user_msgs])
+    txt = app.anthropic.call_citation(system_msg, docs)
+    result = {"data": txt}
+
+    return Response(json.dumps(result), mimetype="application/json", status=200)
